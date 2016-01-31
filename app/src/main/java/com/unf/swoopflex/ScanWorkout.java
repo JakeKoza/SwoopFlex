285baa399ab3e0e1bbb93905f1021d5826ef9748
rlentz1236 <rlentz1236@gmail.com>
2016-01-31 14:34:57 -0500

    Merge branches 'Koza' and 'Richard' of https://github.com/JakeKoza/SwoopFlex into Richard
    # Conflicts:
    #	app/src/main/java/com/unf/swoopflex/MainActivity.java

--- app/src/main/java/com/unf/swoopflex/ScanWorkout.java
+++ app/src/main/java/com/unf/swoopflex/ScanWorkout.java
@@ -5,16 +5,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 /**
  * Created by Ricky on 1/31/16.
  */
 public class ScanWorkout extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.scan_workout, container, false);
     }
-
 
 }
