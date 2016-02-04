c57f96f67ba2acf4a9be1440f3374f006ed90c50
Jake <jakekoza13@gmail.com>
2016-02-04 17:34:58 -0500

    User Input

--- app/src/main/java/com/unf/swoopflex/RandomWorkout.java
+++ app/src/main/java/com/unf/swoopflex/RandomWorkout.java
@@ -3,22 +3,19 @@ package com.unf.swoopflex;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by Jake on 1/21/16.
  */
 public class RandomWorkout extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.random_workout, container, false);
+        return inflater.inflate(R.layout.display_workout, container, false);
     }
 }
