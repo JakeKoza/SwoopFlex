7fcf4ecea502835a3e9345e8bccd2bc8f5ea8d16
rlentz1236 <rlentz1236@gmail.com>
2016-02-07 11:24:10 -0500

    Merge branches 'Koza' and 'Richard' of https://github.com/JakeKoza/SwoopFlex into Richard
    # Conflicts:
    #	app/src/main/java/com/unf/swoopflex/MainActivity.java

--- app/src/main/java/com/unf/swoopflex/RandomWorkout.java
+++ app/src/main/java/com/unf/swoopflex/RandomWorkout.java
@@ -3,13 +3,9 @@ package com.unf.swoopflex;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 /**
  * Created by Jake on 1/21/16.
