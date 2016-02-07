a70a6d52263278f700e7e322992835ca9302c715
rlentz1236 <rlentz1236@gmail.com>
2016-02-07 16:50:33 -0500

    Merge branches 'Koza' and 'Richard' of https://github.com/JakeKoza/SwoopFlex into Richard
    # Conflicts:
    #	app/src/main/java/com/unf/swoopflex/MainActivity.java

--- app/src/main/java/com/unf/swoopflex/HomeWorkout.java
+++ app/src/main/java/com/unf/swoopflex/HomeWorkout.java
@@ -1,37 +1,81 @@
 package com.unf.swoopflex;
 
 
-import android.content.DialogInterface;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 /**
  * Created by Ricky on 1/31/16.
  */
-public class HomeWorkout extends Fragment implements View.OnClickListener {
+public class HomeWorkout extends Fragment{
+
+    Context ctx = null;
     Button btn;
+    Spinner Height_Feet;
+    Spinner Height_Inch;
+    EditText Weight;
+    Spinner Age;
+    RadioGroup Gender;
+    int user_height, user_feet, user_inch, user_weight, user_age, user_gender;
+    double bmi;
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.client_information_form, container, false);
 
-        btn = (Button) view.findViewById(R.id.btnSubmit);
-        btn.setOnClickListener(this);
+        Height_Feet = (Spinner) view.findViewById(R.id.spnFeet);
+        Height_Inch = (Spinner) view.findViewById(R.id.spnInch);
+        Weight = (EditText) view.findViewById(R.id.nmbrWeight);
+        Age = (Spinner) view.findViewById(R.id.spnAge);
+        Gender = (RadioGroup) view.findViewById(R.id.radGender);
+        btn = (Button) view.findViewById(R.id.btnSaveInfo);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_feet = Integer.parseInt(Height_Feet.getSelectedItem().toString());
+                user_inch = Integer.parseInt(Height_Inch.getSelectedItem().toString());
+                user_height = user_inch +(user_feet * 12);
+                user_weight = Integer.parseInt(Weight.getText().toString());
+                user_age = Integer.parseInt(Age.getSelectedItem().toString());
+
+                int genderID = Gender.getCheckedRadioButtonId();
+                if (genderID == -1){
+                    user_gender = 0;
+                }
+                if (genderID == R.id.radioButtonFemale){
+                    user_gender = 0;
+                }
+                if (genderID == R.id.radioButtonMale){
+                    user_gender = 1;
+                }
+                //calculate bmi
+                bmi = 1.5;
+
+                ctx = getActivity().getApplicationContext();
+                SQLiteDB DB = new SQLiteDB(ctx);
+                DB.SQLiteUserInsert(DB, user_height, user_weight, user_age, user_gender, bmi);
+                Toast.makeText(getActivity(), "Data Saved", Toast.LENGTH_LONG).show();
+            }
+        });
 
         return view;
     }
 
 
-    @Override
-    public void onClick(View v) {
-        Toast.makeText(this.getActivity(),
-                "Information Saved", Toast.LENGTH_LONG).show();
-    }
 }
+
+
+
 
---
+++ app/src/main/java/com/unf/swoopflex/SQLiteDB.java
@@ -0,0 +1,50 @@
+package com.unf.swoopflex;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+/**
+ * Created by Ricky on 2/7/2016.
+ */
+public class SQLiteDB extends SQLiteOpenHelper {
+    public static final int database_version = 1;
+    public String CREATE_QUERY = "CREATE TABLE "+ SQLiteData.SQLiteUserTableInfo.TABLE_NAME +"( "+ SQLiteData.SQLiteUserTableInfo.USER_HEIGHT +
+            " INTEGER, "+ SQLiteData.SQLiteUserTableInfo.USER_WEIGHT +" INTEGER, "+ SQLiteData.SQLiteUserTableInfo.USER_AGE +" INTEGER, "+
+            SQLiteData.SQLiteUserTableInfo.USER_GENDER +" INTEGER, "+ SQLiteData.SQLiteUserTableInfo.USER_BMI +" REAL);";
+
+    public SQLiteDB(Context context) {
+        super(context, SQLiteData.SQLiteUserTableInfo.DATABASE_NAME, null, database_version);
+        Log.d("SQLiteDB", "Database Created");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sdb) {
+
+        sdb.execSQL(CREATE_QUERY);
+        Log.d("SQLiteDB", "Table Created");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    public void SQLiteUserInsert(SQLiteDB db, int height, int weight, int age, int gender, double bmi){
+
+        SQLiteDatabase SQ = db.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(SQLiteData.SQLiteUserTableInfo.USER_HEIGHT, height);
+        cv.put(SQLiteData.SQLiteUserTableInfo.USER_WEIGHT, weight);
+        cv.put(SQLiteData.SQLiteUserTableInfo.USER_AGE, age);
+        cv.put(SQLiteData.SQLiteUserTableInfo.USER_GENDER, gender);
+        cv.put(SQLiteData.SQLiteUserTableInfo.USER_BMI, bmi);
+
+        long k = SQ.insert(SQLiteData.SQLiteUserTableInfo.TABLE_NAME, null, cv);
+        Log.d("SQLiteDB", "Info Inserted");
+
+    }
+}
